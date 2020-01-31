bb0b8c0e0ed32324368f18688511518f8dae500c
Federico <federico.caiello@gmail.com>
2020-01-31 18:29:37 +0100

    creazione funzione

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
 	var url = 'https://flynn.boolean.careers/exercises/api/array/music';
+
+
   $.ajax(
     {
       'url': url,
       'method': 'GET',
-      'succes':function (data) {
+      'succes': function (data) {
         console.log(data);
-
+        processData(data.response);
       },
-      'error': function (request, state, error) {
+      'error': function (request, state, errors) {
         alert('errore' + errors);
       }
     }
-  )
+  );
 });
+
+function processData (data) {
+  console.log(data);
+
+}
