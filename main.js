e686a0f53d3dbcd949f0a885ab88dc4cedacee9d
Federico <federico.caiello@gmail.com>
2020-01-31 18:00:29 +0100

    chiamata ajax con api

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	//Code
+	var url = 'https://flynn.boolean.careers/exercises/api/array/music';
+  $.ajax(
+    {
+      'url': url,
+      'method': 'GET',
+      'succes':function (data) {
+        console.log(data);
+
+      },
+      'error': function (request, state, error) {
+        alert('errore' + errors);
+      }
+    }
+  )
 });
