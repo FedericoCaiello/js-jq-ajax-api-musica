83d06b086abbc103e3538d8bde2e8dab7b1dc482
Federico <federico.caiello@gmail.com>
2020-02-02 19:02:54 +0100

    selezione cd non funzionante

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	var url = 'https://flynn.boolean.careers/exercises/api/array/music';
-
-
   $.ajax(
     {
       'url': url,
@@ -13,18 +11,43 @@ $(document).ready(function() {
       'error': function (request, state, errors) {
         alert('errore' + errors);
       }
-    }
-  );
+    });
+	// funzione per ricerca album
+	// $(".selezione").on("click", function() {
+	// 	var active = $('select').val();
+	// 	$.ajax(
+	//     {
+	//       'url': url,
+	//       'method': 'GET',
+	//       'success': function (data) {
+	// 				for (var i=0; i < data.response; i++) {
+	// 					if (data.response[i] == active) {
+	// 						var cd = data[i];
+	// 						var source = $('#entry-template').html();
+	// 						var template = Handlebars.compile(source);
+	// 						var html = template(cd);
+	// 						$('.cds-container').append(html);
+	// 					}
+	// 					else if (active == "tutto") {
+	// 						processData(data)
+	// 					}
+	// 			  }
+	// 			},
+	// 			error: function (richiesta,stato,errore) {
+  //         alert("errore"+ errore)
+	// 	    }
+	//     });
+	// 	})
 });
 
-function processData (cds) {
+function processData (data) {
   // console.log(data);
-  for (var i=0; i < cds.length; i++) {
-    var cd = cds[i];
-    console.log(cd);
+  for (var i=0; i < data.length; i++) {
+    var cd = data[i];
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     var html = template(cd);
     $('.cds-container').append(html);
   }
+	console.log(data);
 }
