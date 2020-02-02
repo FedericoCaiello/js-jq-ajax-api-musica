d9082dc82f42a9bb59d1d0e16b6027baf3828c4c
Federico <federico.caiello@gmail.com>
2020-02-03 00:57:38 +0100

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -13,35 +13,36 @@ $(document).ready(function() {
       }
     });
 	// funzione per ricerca album
-	// $(".selezione").on("click", function() {
-	// 	var active = $('select').val();
-	// 	$.ajax(
-	//     {
-	//       'url': url,
-	//       'method': 'GET',
-	//       'success': function (data) {
-	// 				for (var i=0; i < data.response; i++) {
-	// 					if (data.response[i] == active) {
-	// 						var cd = data[i];
-	// 						var source = $('#entry-template').html();
-	// 						var template = Handlebars.compile(source);
-	// 						var html = template(cd);
-	// 						$('.cds-container').append(html);
-	// 					}
-	// 					else if (active == "tutto") {
-	// 						processData(data)
-	// 					}
-	// 			  }
-	// 			},
-	// 			error: function (richiesta,stato,errore) {
-  //         alert("errore"+ errore)
-	// 	    }
-	//     });
-	// 	})
+	$(".selezione").on("click", function() {
+		var active = $('select').val();
+		$('.cd').remove();
+		$.ajax(
+	    {
+	      'url': url,
+	      'method': 'GET',
+	      'success': function (data) {
+					for (var i=0; i < data.response.length; i++) {
+						if (data.response[i].genre.toLowerCase() == active) {
+							var cd = data.response[i];
+							var source = $('#entry-template').html();
+							var template = Handlebars.compile(source);
+							var html = template(cd);
+							$('.cds-container').append(html);
+						}
+						else if (active.toLowerCase() == "tutto") {
+							processData(data.response);
+						}
+				  }
+				},
+				error: function (richiesta,stato,errore) {
+          alert("errore"+ errore)
+		    }
+	    });
+		});
 });
 
 function processData (data) {
-  // console.log(data);
+
   for (var i=0; i < data.length; i++) {
     var cd = data[i];
     var source = $('#entry-template').html();
